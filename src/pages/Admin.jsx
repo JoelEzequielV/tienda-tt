fb433c5d9e047e5e2da0575e28f00b5643f05b03
JoelEzequielV <149989959+JoelEzequielV@users.noreply.github.com>
2025-06-26 01:57:36 -0300

    Update Admin.jsx

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -130,7 +130,7 @@ const Admin = () => {
 
             <div className="col-md-6 text-end mb-4 m-4">
               <nav>
-                <ul className="pagination justify-content-end mb-0">
+                <ul className="pagination justify-content-center mb-0">
                   {Array.from({ length: totalPaginas }, (_, i) => i + 1).map((num) => (
                     <li key={num} className={`page-item ${num === paginaActual ? 'active' : ''}`}>
                       <button className="page-link" onClick={() => setPaginaActual(num)}>
